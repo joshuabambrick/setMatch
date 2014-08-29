5b4b2c7c8413f18b5974dd3b4250bfc6ce238dd6
Joshua Bambrick <jpbambrick@gmail.com>
2014-08-29 21:53:44 +0100

    fix #4, add #3

--- setMatch.js
+++ setMatch.js
@@ -34,12 +34,15 @@
     // if `id` is 'inherit' and incoming models have an `id`, matched models in the collection will inherit that id
     // if `id` is 'retain' and incoming models have an `id`, matched models in the collection will retain the original id
     Backbone.Collection.prototype.set = function (incomingModels, options) {
-        var matchedModel,                   // the model which matches the object to match
-            namesToMatch,                   // array of names to match
-            currentModelIndex,              // used in `for` loop
-            objectToMatch = {},             // an object whose properties the matchedModel should match
-            shouldMatchUser,                // used to determine if a property name should match (based on user-defined property names)
-            shouldMatch;                    // modified version to also consider other settings which would change the result
+        var matchedModel,                       // the model which matches the object to match
+            namesToMatch,                       // array of names to match
+            currentModelIndex,                  // used in `for` loop
+            objectToMatch = {},                 // an object whose properties the matchedModel should match
+            shouldMatchUser,                    // used to determine if a property name should match (based on user-defined property names)
+            shouldMatch,                        // modified version to also consider other settings which would change the result
+            directCompare,                      // whether or not the `this.setMatch.match` directly compares two models for equality
+            toRemove = [],                      // items to be removed for `incomingModels` because another match was found later in the array
+            assignedIds = {id: {}, cid: {}};    // each property maps to index in `incomingModels` of most recently processed model to be assigned this (c)id
 
         if (this.setMatch && this.setMatch.active !== false) {
             incomingModels = _.isArray(incomingModels) ? incomingModels : [incomingModels];
@@ -60,16 +63,21 @@
                     // if `namesToMatch` is an array, check for a match, otherwise, return true
                     return _.isArray(namesToMatch) ? _.indexOf(namesToMatch, nameToCompare) !== -1 : true;
                 };
-            } else {
+            } else if (this.setMatch.length === 1) {
                 shouldMatchUser = this.setMatch.match;
+            } else {
+                directCompare = true;
             }
 
-            shouldMatch = function (nameToCompare) {
-                // if `shouldMatchUser(nameToCompare)` is true, check to ensure the case of id (otherwise use its reponse of false)
-                return (shouldMatchUser(nameToCompare)
-                    ? !(nameToCompare === 'id' && (this.setMatch.id === 'inherit' || this.setMatch.id === 'retain'))
-                    : false);
-            };
+
+            if (!directCompare) {
+                shouldMatch = function (nameToCompare) {
+                    // if `shouldMatchUser(nameToCompare)` is true, check to ensure the case of id (otherwise use its reponse of false)
+                    return (shouldMatchUser(nameToCompare)
+                        ? !(nameToCompare === 'id' && (this.setMatch.id === 'inherit' || this.setMatch.id === 'retain'))
+                        : false);
+                };
+            }
 
             // set the cid of incoming items to that of elements matched in the collection
             for (currentModelIndex = 0; currentModelIndex < incomingModels.length; currentModelIndex += 1) {
@@ -77,15 +85,19 @@
                 // make this into a proper model object instance (arbitrary cid assigned)
                 incomingModels[currentModelIndex] = this._prepareModel(incomingModels[currentModelIndex], options);
 
-                // create `objectToMatch` by comparing property names
-                _.each(incomingModels[currentModelIndex].attributes, function (propertyValue, propertyName) {
-                    if (shouldMatch(propertyName)) {
-                        objectToMatch[propertyName] = propertyValue;
-                    }
-                });
+                if (!directCompare) {
+                    // create `objectToMatch` by comparing property names
+                    _.each(incomingModels[currentModelIndex].attributes, function (propertyValue, propertyName) {
+                        if (shouldMatch(propertyName)) {
+                            objectToMatch[propertyName] = propertyValue;
+                        }
+                    });
 
-                // look for match with `objectToMatch`
-                matchedModel = this.findWhere(objectToMatch);
+                    // look for match with `objectToMatch`
+                    matchedModel = this.findWhere(objectToMatch);
+                } else {
+                    matchedModel = this.find(_.bind(this.setMatch.match, this.setMatch, incomingModels[currentModelIndex]));
+                }
 
                 if (matchedModel) {
                     if (incomingModels[currentModelIndex].id != null && matchedModel.id !== incomingModels[currentModelIndex].id) {
@@ -95,14 +107,30 @@
                         } else if (this.setMatch.id === 'retain') {
                             // remove id of new element (uses same concept as my custom `clone` method)
                             incomingModels[currentModelIndex] = new incomingModels[currentModelIndex].constructor(_.omit(incomingModels[currentModelIndex].attributes, 'id'));
+                            
+                            if (assignedIds.id[matchedModel.id] != null) {
+                                toRemove.push(assignedIds.id[matchedModel.id]);
+                            }
+                            
+                            assignedIds.id[matchedModel.id] = currentModelIndex;
                         }
                     }
 
                     // overwrite randomly assigned cid on new element (used by Backbone.Collection.prototype.set to match)
                     // do after the above since `incomingModels[currentModelIndex]` may have changed
                     incomingModels[currentModelIndex].cid = matchedModel.cid;
-                }
 
+                    if (assignedIds.cid[matchedModel.cid] != null) {
+                        toRemove.push(assignedIds.cid[matchedModel.cid]);
+                    }
+                    
+                    assignedIds.cid[matchedModel.cid] = currentModelIndex;
+                }
+            }
+
+            // NOTE: important to loop from end of this array (to prevent other indices from changing)
+            while (toRemove.length) {
+                incomingModels.splice(toRemove.pop(), 1);
             }
         }
 
